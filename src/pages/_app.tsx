17296f8a20bc7ac4a920dae3ae95e981b6b5a0f6
roll1226 <tomo_yasu_4724@icloud.com>
2022-05-15 20:55:55 +0900

    fix: MainLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "the-new-css-reset/css/reset.css";
 import type { AppProps } from "next/app";
-import { MainLayout } from "./Layout/MainLayout";
+import MainLayout from "./Layout/MainLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
